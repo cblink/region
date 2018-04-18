c7b228ce1ba2164bd2f19b75887fced771a57108
HanSon <h@hanc.cc>
2018-04-18 11:39:04 +0800

    修改 迁移

--- src/publishable/database/migrations/2018_01_01_000000_create_regions_table.php
+++ src/publishable/database/migrations/2018_01_01_000000_create_regions_table.php
@@ -15,25 +15,43 @@ class CreateRegionsTable extends Migration
         Schema::create('provinces', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('code')->index();
+            $table->string('code')->nullable()->index();
         });
 
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('province_id')->index();
             $table->string('name');
-            $table->string('code')->index();
+            $table->string('code')->nullable()->index();
         });
 
         Schema::create('areas', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('city_id')->index();
             $table->string('name');
-            $table->string('code')->index();
+            $table->string('code')->nullable()->index();
         });
 
-        // 存储行政编码
-        $this->fillRegionsWithCode();
+        // 存储行政编码 数据源有问题，极力不建议使用
+//        $this->fillRegionsWithCode();
+
+        $this->fillRegionsWithoutCode();
+    }
+
+    public function fillRegionsWithoutCode()
+    {
+        $region = new Region();
+        $provinces = $region->getRegions();
+        foreach ($provinces as $province) {
+            $provinceId = DB::table('provinces')->insertGetId(['name' => $province['name']]);
+            foreach ($province['city'] as $city) {
+                $cityId = DB::table('cities')->insertGetId(['name' => $city['name'], 'province_id' => $provinceId]);
+                $areas = array_map(function ($area) use ($cityId) {
+                    return ['name' => $area, 'city_id' => $cityId];
+                }, $city['area']);
+                DB::table('areas')->insert($areas);
+            }
+        }
     }
 
     public function fillRegionsWithCode()
